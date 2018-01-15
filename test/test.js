12d0d28d3a6f633199f1a8a0d410ff6d82e144a1
えむけー <makoto_16n1200102@nnn.ed.jp>
2018-01-15 14:17:24 +0900

    Mochaのフックをいろいろ試してみました

--- test/test.js
+++ test/test.js
@@ -3,17 +3,43 @@
 const Sequelize = require('sequelize')
 const assert = require('assert')
 
+let sequelize
+
+before(() => {
+  console.log('Root level before')
+  sequelize = new Sequelize('postgres://postgres:postgres@localhost/myapp',
+    { operatorsAliases: false })
+})
+
+after(() => {
+  console.log('Root level after')
+  sequelize.close()
+})
+
+beforeEach(() => {
+  console.log('Root level before each')
+})
+
+afterEach(() => {
+  console.log('Root level after each')
+})
+
 describe('postgresqlに接続するテスト', () => {
 
-  let sequelize
-
   before(() => {
-    sequelize = new Sequelize('postgres://postgres:postgres@localhost/myapp',
-      { operatorsAliases: false })
+    console.log('describe01 before')
   })
 
   after(() => {
-    sequelize.close()
+    console.log('describe01 after')
+  })
+
+  beforeEach(() => {
+    console.log('describe01 before each')
+  })
+
+  afterEach(() => {
+    console.log('describe01 after each')
   })
 
   it('とりあえず接続', done => {
@@ -27,4 +53,36 @@ describe('postgresqlに接続するテスト', () => {
     }).catch(done)
   })
 
+  it('もういっこテスト', () => {
+    assert.ok(true)
+  })
+
 })
+
+describe('フックのテスト', () => {
+  before(() => {
+    console.log('describe02 lbefore')
+  })
+
+  after(() => {
+    console.log('describe02 after')
+  })
+
+  beforeEach(() => {
+    console.log('describe02 before each')
+  })
+
+  afterEach(() => {
+    console.log('describe02 after each')
+  })
+
+  it('さらにテスト', () => {
+    assert.ok(true)
+  })
+
+  it('さらにもういっこテスト', () => {
+    assert.ok(true)
+  })
+
+})
+
