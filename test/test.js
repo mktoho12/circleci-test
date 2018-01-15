70070e3a74e2d7dba42260f2eaa6371ccaf5e214
えむけー <makoto_16n1200102@nnn.ed.jp>
2018-01-15 09:59:07 +0900

    テスト後にDBクローズを呼ぶようにした

--- test/test.js
+++ test/test.js
@@ -1,24 +1,30 @@
 'use strict'
 
 const Sequelize = require('sequelize')
+const assert = require('assert')
 
-describe('postgresqlに接続できるかテスト', () => {
-  it('とりあえず接続', done => {
-    const sequelize = new Sequelize('postgres://postgres:postgres@localhost/myapp',
+describe('postgresqlに接続するテスト', () => {
+
+  let sequelize
+
+  before(() => {
+    sequelize = new Sequelize('postgres://postgres:postgres@localhost/myapp',
       { operatorsAliases: false })
-    const User = sequelize.define('user', {
-      name: Sequelize.STRING,
-      description: Sequelize.TEXT
-    })
+  })
+
+  after(() => {
+    sequelize.close()
+  })
+
+  it('とりあえず接続', done => {
     const Project = sequelize.define('project', {
       title: Sequelize.STRING,
       description: Sequelize.TEXT
     })
     Project.sync().then(() => {
-      User.sync().then(() => {
-        done()
-      })
-    })
+      assert.ok(true)
+      done()
+    }).catch(done)
   })
-})
 
+})
